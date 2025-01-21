51ee67f62e63fa379fc25a4ef213f60fb59d7052
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-01-21 11:51:16 +0200

    develop: new database dump

--- docs/sql/mvc-framework.sql
+++ docs/sql/mvc-framework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: database
--- Generation Time: Jan 16, 2025 at 11:46 AM
+-- Generation Time: Jan 21, 2025 at 09:44 AM
 -- Server version: 10.11.9-MariaDB
 -- PHP Version: 7.4.20
 
@@ -24,6 +24,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `feedback` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `date`, `name`, `email`, `feedback`) VALUES
+(1, '2025-01-20 13:28:33', 'jussi jokinen', 'jussi.jokinen@openinnovations.io', 'keep up the good work');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `excerpt` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `date`, `title`, `excerpt`, `content`, `user_id`) VALUES
+(1, '2025-01-20 14:35:30', 'Welcome to Simple MVC framework', 'This is a simple example how to make an MVC framework', 'This was suppose to be a vanilla/legacy project, but things get out of hand, so here we are.', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,35 +76,73 @@ CREATE TABLE `users` (
   `password` varchar(256) DEFAULT NULL,
   `first_name` varchar(256) DEFAULT NULL,
   `last_name` varchar(256) DEFAULT NULL,
-  `email` varchar(256) DEFAULT NULL
+  `email` varchar(256) DEFAULT NULL,
+  `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `email`) VALUES
-(1, 'jussi.jokinen', 'verysecret', 'Jussi', 'Jokinen', 'jussi.jokinen@openinnovations.io');
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `email`, `title`) VALUES
+(1, 'jussi.jokinen', 'verysecret', 'Jussi', 'Jokinen', 'jussi.jokinen@openinnovations.io', 'Script kiddie and a sunday coder');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_news` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_email` (`email`),
+  ADD UNIQUE KEY `unique_username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `fk_user_news` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
