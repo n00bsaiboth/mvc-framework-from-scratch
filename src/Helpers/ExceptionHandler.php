4b326955c938e5bce146d7fd8ac86c20f257005d
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2025-01-21 11:53:24 +0200

    develop: added write to log functionality in own method

--- src/Helpers/ExceptionHandler.php
+++ src/Helpers/ExceptionHandler.php
@@ -8,11 +8,7 @@ class ExceptionHandler {
     public static function handle(Throwable $exception): void {
         error_log($exception->getMessage());
 
-        file_put_contents(
-            __DIR__ . '../../../logs/errors.log',
-            '[' . date('d-m-Y H:i:s') . '] ' . $exception->getMessage() . PHP_EOL,
-            FILE_APPEND
-        );
+        self::writeToLog($exception);
 
         if (php_sapi_name() === 'cli') {
             echo "An error occured: " . $exception->getMessage() . PHP_EOL;
@@ -20,5 +16,13 @@ class ExceptionHandler {
             http_response_code(500);
             echo "Sorry, something went wrong.";
         }
+    }
+
+    public static function writeToLog($exception): void {
+        file_put_contents(
+            __DIR__ . '../../../logs/errors.log',
+            '[' . date('d-m-Y H:i:s') . '] ' . $exception->getMessage() . PHP_EOL,
+            FILE_APPEND
+        );
     }
 }
